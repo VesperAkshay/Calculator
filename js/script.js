457111bfc6ed39ac4e151269b08f38dab857eaa3
Anshika <akshay@gmail.com>
2023-09-15 10:43:00 +0530

    CAlculator Webpage

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let currentOperator = '';
+
+function appendToDisplay(value) {
+    currentInput += value;
+    display.value = currentInput;
+}
+
+function clearDisplay() {
+    currentInput = '';
+    display.value = '';
+}
+
+function calculateResult() {
+    try {
+        let result = eval(currentInput);
+        display.value = result;
+        currentInput = result.toString();
+    } catch (error) {
+        display.value = 'Error';
+        currentInput = '';
+    }
+}
